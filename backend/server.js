737fad2394aa77674fefcd2ae88850c371be27d3
BuildTools <unconfigured@null.spigotmc.org>
2024-02-23 19:45:53 -0500

    Got backend running woohoo

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+/*
+User guide:
+    Starting the server: Open the backend directory in your terminal / command line,
+    and run "nodemon server.js"
+
+    Connecting to the server: Open the URL "localhost:8000" in your browser.
+ */
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Connected to server. Welcome to the QVSA server!");
+});
+
+const port = 8000;
+
+// Listen for incoming requests on localhost:8000
+app.listen(port, () => {
+    console.log("Server listening on port " + port + "!");
+})

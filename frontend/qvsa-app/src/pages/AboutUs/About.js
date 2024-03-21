82ee1099c820d171e9fdd3e05af40f2494cbee08
kale <18kmkf@queensu.ca>
2024-03-21 18:24:43 -0400

    Add header

--- frontend/qvsa-app/src/pages/AboutUs/About.js
+++ frontend/qvsa-app/src/pages/AboutUs/About.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../../components/Header";
 
 const About = () => {
   return (
     <div>
+      <Header title="About Us" year="2024-2025"/>
       <h1>About Us Page</h1>
     </div>
   );

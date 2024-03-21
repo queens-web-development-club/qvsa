48a81cc0af3b6f11784c0b979bee0f9eb937b4c1
kale <18kmkf@queensu.ca>
2024-03-21 18:19:20 -0400

    init events file

--- frontend/qvsa-app/src/pages/Events/Events.js
+++ frontend/qvsa-app/src/pages/Events/Events.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Header from "../../components/Header";
 
 const Events = () => {
   return (
-    <div>
-      <h1>Events Page</h1>
+    <div className="h-auto">
+      <div className="h-1/2">
+        <Header title="Events" year="2024-2025" />
+      </div>
+      <h3 className="text-white text-center p-8">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </h3>
     </div>
   );
 };

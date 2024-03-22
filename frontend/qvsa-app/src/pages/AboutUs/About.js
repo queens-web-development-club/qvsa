93d6603cfdedb7871bd7318c48159ac2f65b2813
Annika T <149737348+annika-t@users.noreply.github.com>
2024-03-22 12:36:28 -0400

    aboutpage section 1

--- frontend/qvsa-app/src/pages/AboutUs/About.js
+++ frontend/qvsa-app/src/pages/AboutUs/About.js
@@ -1,11 +1,34 @@
 import React from "react";
 import Header from "../../components/Header";
+import GroupPic from "..//..//assets/aboutus1.jpg";
 
 const About = () => {
   return (
     <div>
-      <Header title="About Us" year="2024-2025"/>
-      <h1>About Us Page</h1>
+      <Header title="About Us" year="2024-2025" />
+      <div className=" flex flex-col justify-center items-center">
+        <div className="w-4/6">
+          <div className="flex flex-col justify-center items-center gap-9">
+            <h2 className="text-3xl text-white font-bold text-center mt-16">
+              Who Are We
+            </h2>
+            <p className="text-white text-center text-xl">
+              The Vietnamese Student Association (VSA) is a close-knit community
+              of students and enthusiasts dedicated to celebrating and sharing
+              the beauty of Vietnamese culture. We organize vibrant cultural
+              events, educational workshops, and social gatherings that not only
+              showcase our rich traditions but also create a welcoming space for
+              all. Our goal is to foster a sense of belonging and pride in our
+              heritage while promoting cultural diversity and understanding on
+              campus. Join us as we embark on a journey of discovery,
+              friendship, and cultural exchange!
+            </p>
+            <img src={GroupPic} alt="Group Pic" className="size-3/6" />
+          </div>
+
+          <div>section 2</div>
+        </div>
+      </div>
     </div>
   );
 };

bcbfeab9129cb726b9e6d70ab37686f40f0c56d4
kale <18kmkf@queensu.ca>
2024-03-27 13:50:39 -0400

    Update landing page image

--- frontend/qvsa-app/src/pages/Home/Home.js
+++ frontend/qvsa-app/src/pages/Home/Home.js
@@ -2,14 +2,18 @@ import React from "react";
 import AboutUs from "./AboutUs";
 import Events from "./Events";
 import MeetTheTeam from "./MeetTheTeam";
+import logo from "..//..//assets/logo.png";
 
 const Home = () => {
   return (
     <>
-      <div className="flex flex-col items-center min-h-screen overflow-hidden bg-home-background bg-center bg-clip-border">
-        <h1 className="text-center text-white text-6xl font-inter font-bold mt-32">
-          Queen’s Vietnamese Student Association
-        </h1>
+      <div className="flex flex-col items-center h-screen bg-home-background bg-cover bg-center">
+        <div className="flex flex-row items-center gap-6 mt-60">
+          <img src={logo} alt="logo" className="h-20" />
+          <h1 className="text-center text-white text-5xl font-inter font-bold">
+            Queen’s Vietnamese Student Association
+          </h1>
+        </div>
       </div>
       <AboutUs />
       <Events />

fd977eb78dea4d3e26163a21fef36ae430d02da4
kavitathom <kavita.t@queensu.ca>
2024-04-02 18:14:58 -0400

    nn

--- frontend/qvsa-app/src/pages/MeetTeam/Team.js
+++ frontend/qvsa-app/src/pages/MeetTeam/Team.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import memberProfile from './memberProfile.jpg';
+import teamComponents from "./teamComponents";
 
 import Duy from './Duy.jpg';
 import Kate from './Kate.jpg';
@@ -13,92 +13,10 @@ import Ethan from './Ethan.jpg';
 import Kevin from './Kevin.jpg';
 import Samantha from './Samantha.jpg'
 
-//import nav bar, top city photo component with 'Meet the Team' text, and the footer at bottom
-
-// functional component for bordered img
-const BorderedImage = ({ src, alt }) => (
-  <div className="border-8 border-solid border-yellow bg-center bg-no-repeat" style={{ width: '250px', height: 'auto' }} >
-    <img  img src={src} alt={alt} />
-  </div>
-);
 
 
-// functional component for caption
-const Caption = ({ name, role }) => (
-  <div>
-    <div className="text-white text-center text-2xl font-bold mt-2"> 
-      <p> {name} </p>
-    </div>
-    <div className="text-white text-center text-2xl font-regular mt-2">
-      <p> {role} </p>
-    </div>
-  </div>  
-);
 
-
-// functional component for position title ex/ president, design director, marketing director etc.
-const Title = ({ title }) => (
-  <div>
-    <h1 className="text-white font-bold text-4xl text-center mb-10">
-      {title}
-    </h1>
-    <div className="p-2"/>
-  </div>
-);
-
-
-// functional component for TwoBin image row
-// uses BorderedImage and Caption components 
-const TwoBin = ({ memberProfile1, memberProfile2, name1, name2, role1, role2 }) => (
-  <div className="flex justify-center">
-    <div className="mr-20 mb-10" > {/* start div of first col with img and text */}
-      <div className="flex flex-col items-center"> 
-        <BorderedImage src={memberProfile1} alt={`${name1}, ${role1}`} />
-        <div className="p-2"/>
-        <Caption name={name1} role={role1} />
-      </div>
-    </div> 
-    <div className="mb-10" > {/* start div of second col with img and text */}
-      <div className="flex flex-col items-center"> 
-        <BorderedImage src={memberProfile2} alt={`${name2}, ${role2}`} />
-        <div className="p-2"/>
-        <Caption name={name2} role={role2} />
-      </div>
-    </div> 
-  </div>
-);
-
-
-// functional component for ThreeBin image row
-// uses BorderedImage and Caption components
-const ThreeBin = ({ memberProfile1, memberProfile2, memberProfile3, name1, name2, name3, role1, role2, role3 }) => (
-  <div className="flex justify-center">
-    <div className="mr-20 mb-10" > {/* start div of first col with img and text */}
-      <div className="flex flex-col items-center"> 
-        <BorderedImage src={memberProfile1} alt={`${name1}, ${role1}`} />
-        <div className="p-2"/>
-        <Caption name={name1} role={role1} />
-      </div>
-    </div>
-    <div className="mr-20 mb-10" > {/* start div of first col with img and text */}
-      <div className="flex flex-col items-center"> 
-        <BorderedImage src={memberProfile2} alt={`${name2}, ${role2}`} />
-        <div className="p-2"/>
-        <Caption name={name2} role={role2} />
-      </div>
-    </div>
-    <div className="mb-10" > {/* start div of second col with img and text */}
-      <div className="flex flex-col items-center"> 
-        <BorderedImage src={memberProfile3} alt={`${name3}, ${role3}`} />
-        <div className="p-2"/>
-        <Caption name={name3} role={role3} />
-      </div>
-    </div> 
-  </div>
-);
-
-
-// build UI using components
+// build UI using components, everything hard coded asfrn due to backend being incomplete
 const Team = () => {
   return(
     <div className="bg-black">

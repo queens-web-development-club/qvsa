e5eacafcace93619ea763282da50809be9de7baf
seanzinner <20spz@queensu.ca>
2024-04-03 11:29:04 -0400

    Creating Components and fixing footer

--- frontend/qvsa-app/src/components/Footer.js
+++ frontend/qvsa-app/src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
 
 
-    <footer className="bg-[#2E2E30] text-white py-7 border-t-2 border-white">
+    <footer className="bg-[#2E2E30] text-white pt-4">
 
       <div className="flex pl-8 text-[#F2F2F2] ">
        <img src={qvsaLotusIcon} alt="Logo" className="mr-4 w-62 h-38 object-contain" />
@@ -18,13 +18,13 @@ const Footer = () => {
       <div>
        <ul className="flex flex-col px-4">
         <li className="flex">
-          <img src={emailLogo} alt="EmailLogo" className="pt-20 pl-6 w-47 h-33 object-contain"/>
-          <a href="mailto:qvsa2022@gmail.com" className="pt-20 pl-7 pr-5 font-Inter text-base hover:text-[#FFB90B] font-semibold underline">qvsa2022@gmail.com </a>
+          <img src={emailLogo} alt="EmailLogo" className="pt-8 pl-6 w-47 h-33 object-contain"/>
+          <a href="mailto:qvsa2022@gmail.com" className="pt-8 pl-7 pr-5 font-Inter text-base hover:text-[#FFB90B] font-semibold underline">qvsa2022@gmail.com </a>
         </li>
 
-        <li className="flex pb-8 justify-left">
-          <img src={instaLogo} alt="Instagram Logo" className="pt-8 pl-6 w-25 h-33 object-contain"/>
-          <a href="https://www.instagram.com/queensvsa/" className="pt-10 pl-7 pr-5 font-Inter text-base hover:text-[#FFB90B] font-semibold underline">@queensvsa</a>
+        <li className="flex pb-4 justify-left">
+          <img src={instaLogo} alt="Instagram Logo" className="pt-4 pl-6 w-25 h-33 object-contain"/>
+          <a href="https://www.instagram.com/queensvsa/" className="pt-4 pl-7 pr-5 font-Inter text-base hover:text-[#FFB90B] font-semibold underline">@queensvsa</a>
         </li>
       
         </ul>

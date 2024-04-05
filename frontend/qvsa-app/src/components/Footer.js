1d02a4254ea9c62bf4dc9627174f2815b9984753
seanzinner <20spz@queensu.ca>
2024-04-04 20:17:02 -0400

    New navbar edits and other stuff

--- frontend/qvsa-app/src/components/Footer.js
+++ frontend/qvsa-app/src/components/Footer.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import qvsaLotusIcon from "../assets/qvsa_lotus_icon.png";
 import emailLogo from "../assets/emailLogo.png";
 import instaLogo from "../assets/instaLogo.png";
@@ -8,24 +9,32 @@ const Footer = () => {
   return (
 
 
-    <footer className="bg-[#2E2E30] text-white pt-4">
+    <footer className="bg-[#2E2E30] text-white pt-14">
 
-      <div className="flex pl-8 text-[#F2F2F2] ">
-       <img src={qvsaLotusIcon} alt="Logo" className="mr-4 w-62 h-38 object-contain" />
-        <h1 className="font-Inter text-2xl md:mb-0 mb-6 lg:leading-normal font-semibold"> Queen's Vietnamese Student Association</h1>
+      <div className="flex pl-10 text-[#F2F2F2] ">
+       <img src={qvsaLotusIcon} alt="Logo" className="mr-4 w-8 object-contain" />
+        <h1 className="font-Inter text-xl md:mb-0 mb-6 lg:leading-normal font-bold"> Queen's Vietnamese Student Association</h1>
       </div>
       
       <div>
        <ul className="flex flex-col px-4">
-        <li className="flex">
-          <img src={emailLogo} alt="EmailLogo" className="pt-8 pl-6 w-47 h-33 object-contain"/>
-          <a href="mailto:qvsa2022@gmail.com" className="pt-8 pl-7 pr-5 font-Inter text-base hover:text-[#FFB90B] font-semibold underline">qvsa2022@gmail.com </a>
+       <li className="flex pb-4 justify-left">
+          <img src={instaLogo} alt="Instagram Logo" className="pt-6 pl-8 w-14 object-contain"/>
+          <a href="https://www.instagram.com/queensvsa/" className="pt-6 pl-4 pr-5 font-Inter text-base hover:text-[#FFB90B] underline">@queensvsa</a>
         </li>
+        
+        <div className="flex">
+        <li className="flex w-1/2">
+          <img src={emailLogo} alt="EmailLogo" className="pl-8 w-14 pt-3 object-contain"/>
+          <a href="mailto:qvsa2022@gmail.com" className="pl-4 pr-5 pt-5 font-Inter text-base hover:text-[#FFB90B] underline">qvsa2022@gmail.com </a>
+        </li>
+        
+        <li className='pt-8 pl-8 pr-10 w-1/2 text-right'>
+          <Link to="/admin" className='font-Inter text-base hover:text-[#FFB90B] underline pb-8'>Login</Link>
+        </li>
+        </div>
 
-        <li className="flex pb-4 justify-left">
-          <img src={instaLogo} alt="Instagram Logo" className="pt-4 pl-6 w-25 h-33 object-contain"/>
-          <a href="https://www.instagram.com/queensvsa/" className="pt-4 pl-7 pr-5 font-Inter text-base hover:text-[#FFB90B] font-semibold underline">@queensvsa</a>
-        </li>
+
       
         </ul>
       </div>
--- frontend/qvsa-app/src/pages/Events/Events.js
+++ frontend/qvsa-app/src/pages/Events/Events.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { allEventsData } from './eventsData';
 import EventCard from './EventCard';
 import AlternateEventCard from './AlternateEventCard';
+import Header from '../../components/Header.js'
 
 const Events = () => {
   return (
     <div>
-      <div className="flex flex-col bg-cover bg-center bg-citySky bg-opacity-50 max-h-screen w-full font-bold font-Inter flex items-center">
-        <h1 className="text-[#FFB90B] text-6xl text-center pt-20 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"> Events</h1>
-        <p className="text-3xl text-center m-2 pb-10 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"> 2024 - 2025 </p>
-      </div>
+
+      <Header title='Events' year='2024-2025' />
 
       <div className="flex w-full font-bold font-Inter justify-center">
         <p className="text-[#F2F2F2] text-3xl text-center p-16"> Past Events </p>

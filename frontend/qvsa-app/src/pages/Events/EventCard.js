72fbd13d5300c6b419551413ed525b53e241f26e
kale <18kmkf@queensu.ca>
2024-07-21 14:07:04 -0400

    Update event cards

--- frontend/qvsa-app/src/pages/Events/EventCard.js
+++ frontend/qvsa-app/src/pages/Events/EventCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const EventCard = ({ data }) => {
   const { title, description, images, thumbnails, year, month } = data;
@@ -12,14 +12,22 @@ const EventCard = ({ data }) => {
   return (
     <div className="flex flex-col md:flex-row border-t-2 border-white ml-2 md:ml-12 justify-center md:justify-end mb-6 w-auto">
       <div className="w-full md:w-4/12 p-4">
-        <h1 className="font-Inter font-bold text-[#F2F2F2] text-4xl m-4 text-center md:text-right md:pl-3 md:break-words"><span className="text-[#FFB90B] md:hidden block md:hyphens-manual"> {month}, {year} </span> {title}</h1>
-        <p className="font-Inter text-[#F2F2F2] text-lg text-center md:text-right m-4 pb-12 border-b-2 border-white md:break-words md:pl-3">{description}</p>
+        <h1 className="font-Inter font-bold text-[#F2F2F2] text-4xl m-4 text-center md:text-right md:pl-3 md:break-words">
+          <span className="text-[#FFB90B] md:hidden block md:hyphens-manual">
+            {" "}
+            {month}, {year}{" "}
+          </span>{" "}
+          {title}
+        </h1>
+        <p className="font-Inter text-[#F2F2F2] text-lg text-center md:text-right m-4 pb-12 border-b-2 border-white md:break-words md:pl-3">
+          {description}
+        </p>
       </div>
 
       <div className="mb-4 p-6 pb-16 w-full md:w-auto">
         <div className="w-full md:w-[50vw] h-[50vh] overflow-hidden mx-auto">
-          <img 
-            src={images[currentIndex]} 
+          <img
+            src={images[currentIndex]}
             alt={`Image ${currentIndex + 1}`}
             className="w-full h-full object-cover border-4 border-[#FFB90B]"
           />
@@ -28,16 +36,24 @@ const EventCard = ({ data }) => {
         <div className="w-full md:w-auto mt-4 md:mt-0">
           <div className="flex mt-4 justify-center md:justify-start">
             {thumbnails.map((thumbnail, index) => (
-              <a 
-                key={index} 
-                href={images[index]} 
-                className={`btn btn-xs ${index === currentIndex ? 'btn-primary border-4 border-[#FFB90B] transition duration-300 opacity-100' : 'btn-secondary border-4 border-white opacity-50'} hover:border-[#FFB90B] transition duration-300 mr-4 sm:mr-6 mt-6`}
-                onClick={(e) => { 
+              <a
+                key={index}
+                href={images[index]}
+                className={`btn btn-xs ${
+                  index === currentIndex
+                    ? "btn-primary border-4 border-[#FFB90B] transition duration-300 opacity-100"
+                    : "btn-secondary border-4 border-white opacity-50"
+                } hover:border-[#FFB90B] transition duration-300 mr-4 sm:mr-6 mt-6`}
+                onClick={(e) => {
                   e.preventDefault();
                   showImage(index);
                 }}
               >
-                <img src={thumbnail} alt={`Thumbnail ${index + 1}`} className="w-28 h-28 object-cover" />
+                <img
+                  src={thumbnail}
+                  alt={`Thumbnail ${index + 1}`}
+                  className="w-28 h-28 object-cover"
+                />
               </a>
             ))}
           </div>
@@ -45,11 +61,14 @@ const EventCard = ({ data }) => {
       </div>
 
       <div className="w-full md:w-auto mt-4 md:mt-0 hidden md:block">
-        <p className="border-2 border-white px-4 text-[#FFB90B] text-lg text-center font-bold">{year}</p>
-        <p className="border-2 border-white p-8 text-[#FFB90B] text-4xl text-center font-bold">{month}</p>
+        <p className="border-2 border-white px-4 text-[#FFB90B] text-lg text-center font-bold">
+          {year}
+        </p>
+        <p className="border-2 border-white p-8 text-[#FFB90B] text-4xl text-center font-bold">
+          {month}
+        </p>
       </div>
     </div>
-
   );
 };
 
--- frontend/qvsa-app/src/pages/Events/eventsData.js
+++ frontend/qvsa-app/src/pages/Events/eventsData.js
@@ -4,74 +4,30 @@ import newYear2 from "../../assets/newYear2.png";
 import newYear3 from "../../assets/newYear3.png";
 import newYear4 from "../../assets/newYear4.png";
 
-  const images = [
-    newYear1,
-    newYear2,
-    newYear3,
-    newYear4
-  ];
+const images = [newYear1, newYear2, newYear3, newYear4];
 
-  const thumbnails = [
-    newYear1,
-    newYear2,
-    newYear3,
-    newYear4
-  ];
+const thumbnails = [newYear1, newYear2, newYear3, newYear4];
 
 export const allEventsData = [
-    {
-      title: "Tết, Vietnamese Lunar New Year",
-      description: "Tết, or Vietnamese Lunar New Year, is a vibrant and joyous celebration...",
-      images: [newYear1, newYear2, newYear3, newYear4],
-      currentIndex: 0,
-      thumbnails: [newYear1, newYear2, newYear3, newYear4],
-      year: "2023",
-      month: "Feb"
-    },
-    {
-        title: "Tết, Vietnamese Lunar New Year",
-        description: "Tết, or Vietnamese Lunar New Year, is a vibrant and joyous celebration...",
-        images: [newYear1, newYear2, newYear3, newYear4],
-        currentIndex: 0,
-        thumbnails: [newYear1, newYear2, newYear3, newYear4],
-        year: "2023",
-        month: "Feb"
-      },
-      {
-        title: "Tết, Vietnamese Lunar New Year",
-        description: `Tết, or Vietnamese Lunar New Year, is a vibrant and joyous celebration that 
-                      marks the arrival of spring and new beginnings. Families come together to 
-                      clean their homes and prepare special dishes, such as bánh chưng 
-                      (sticky rice cake) and bánh dày (round rice cake), to offer to 
-                      their ancestors. The streets are filled with colorful flowers, 
-                      especially peach blossoms and kumquat trees, symbolizing prosperity and 
-                      good fortune. People exchange gifts, visit friends and relatives, and 
-                      enjoy traditional performances like lion dances and firecrackers.`,
-
-        images: [newYear1, newYear2, newYear3, newYear4],
-        currentIndex: 0,
-        thumbnails: [newYear1, newYear2, newYear3, newYear4],
-        year: "2023",
-        month: "Feb"
-      },
-      {
-        title: "Tết, Vietnamese Lunar New Year",
-        description: "Tết, or Vietnamese Lunar New Year, is a vibrant and joyous celebration...",
-        images: [newYear1, newYear2, newYear3, newYear4],
-        currentIndex: 0,
-        thumbnails: [newYear1, newYear2, newYear3, newYear4],
-        year: "2023",
-        month: "Feb"
-      },   
-      {
-        title: "Tết, Vietnamese Lunar New Year",
-        description: "Tết, or Vietnamese Lunar New Year, is a vibrant and joyous celebration...",
-        images: [newYear1, newYear2, newYear3, newYear4],
-        currentIndex: 0,
-        thumbnails: [newYear1, newYear2, newYear3, newYear4],
-        year: "2023",
-        month: "Feb"
-      },
-    // Add more events as needed
-  ];
-  
+  {
+    title: "Tết, Vietnamese Lunar New Year",
+    description:
+      "To celebrate Tết, or Vietnamese Lunar New Year, we hosted eventful night of traditional games and Vietnamese cultural foods",
+    images: [newYear1, newYear2, newYear3, newYear4],
+    currentIndex: 0,
+    thumbnails: [newYear1, newYear2, newYear3, newYear4],
+    year: "2024",
+    month: "January",
+  },
+  {
+    title: "Halloween Party",
+    description:
+      "We hosted a chilling Halloween Party Night filled with delicious treats and various game stations, such as Murder Mystery and Mafia.",
+    images: [newYear1, newYear2, newYear3, newYear4],
+    currentIndex: 0,
+    thumbnails: [newYear1, newYear2, newYear3, newYear4],
+    year: "2023",
+    month: "November",
+  },
+  // add more events as needed
+];

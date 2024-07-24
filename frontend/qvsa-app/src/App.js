8f35bc56eb94a63c9f849e2ba1ea06588ef82248
kale <18kmkf@queensu.ca>
2024-07-23 20:55:08 -0400

    Update favicon and title

--- frontend/qvsa-app/src/App.js
+++ frontend/qvsa-app/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { About, ContactUs, Events, Home, Team, AdminPanel } from "./pages";
+import { About, ContactUs, Events, Home, Team } from "./pages";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -13,7 +13,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="/admin" element={<AdminPanel />} />
           <Route path="/events" element={<Events />} />
           <Route path="/meet-the-team" element={<Team />} />
         </Routes>
